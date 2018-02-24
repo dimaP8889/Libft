5642da2e3c7f66fa72003bd35f3bc593d9f168f4
Dmytro POGREBNIAK <dpogrebn@e1r9p2.unit.ua>
2018-02-24 19:09:35 +0200

    printf

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "includes/get_next_line.h"
 
 static void		ft_make_buf(t_gnl *new)
 {
